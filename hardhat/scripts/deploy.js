a31f25a8351c763c20c76e3c570bcd0d19a68735
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-09-29 17:08:02 +0545

    nft contract adddress

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -17,9 +17,9 @@ async function main() {
     fakeNftMarketplace.address,
     CRYPTODEVS_NFT_CONTRACT_ADDRESS,
     {
-      // This assumes your account has at least 1 ETH in it's account
+      // This assumes your account has at least 0.01 ETH in it's account
       // Change this value as you want
-      value: ethers.utils.parseEther("1"),
+      value: ethers.utils.parseEther("0.01"),
     }
   );
   await cryptoDevsDAO.deployed();
